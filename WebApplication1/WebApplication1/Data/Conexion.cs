fdc66b07cfa58a8399beb3a984be33df47211a08
FranciscoLolmet <36779013+FranciscoLolmet@users.noreply.github.com>
2024-07-25 22:41:53 -0600

    API_inicial

---
+++ WebApplication1/WebApplication1/Data/Conexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Data
+{
+    public class Conexion
+    {
+        //"Data Source=.;Initial Catalog=DBPRUEBAS;Integrated Security=True" **Data Source=.;Initial Catalog=DBPRUEBAS;Integrated Security=True** ;Trust Server Certificate=True
+        public static string rutaConexion = "Data Source=DESKTOP-3U35QIE\\SQLEXPRESS;Initial Catalog=DBPRUEBAS;Integrated Security=True";
+    }
+}
